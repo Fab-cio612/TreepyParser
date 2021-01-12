6a00eccc18f01a51eef7004d4813c9e5cfd7917a
Fabio <f.cioni612@gmail.com>
2021-01-12 18:14:31 +0100

    add explanation to parser

--- TreepyParser/parser.py
+++ TreepyParser/parser.py
@@ -15,7 +15,7 @@ def parse_attributes(string):
                 val = True
             else:
                 val = False
-                attr[curr_key.lstrip()] = string[lagging_pointer + 1: pointer]
+                attr[curr_key.strip()] = string[lagging_pointer + 1: pointer]
 
             lagging_pointer = pointer
         if c == '=':
@@ -110,3 +110,61 @@ def parse(html):
 
             pointer +=1
     return stack[0].nodes[0]
+
+#how the parser works:
+#
+#We have a stack to keep track which element we are in currently
+#e.g.
+#
+#<div>
+#   <p>
+#       ...inside a paragraph...
+#   </p>
+#</div>
+#
+#We have two pointers (lagging pointer and pointer) to slice parts of the html string
+#e.g.   ()... is the pointer position
+#
+#<p(>)Example Text(<)/P>        we can use html[lagging_pointer + 1: pointer] to get the text inside the <p> element
+#
+#the pointer points to the current character, while the lagging pointer only changes when states change
+#
+#There are five states:
+#0 - Traversing:
+#   -is active when we are outside a tag
+#   -as soon as a tag is detected we switch to state 1
+#   -we create a text node with the text between the two pointers and add it to the node on top of the stack
+#   -lagging pointer is set to the current character which is the opening bracket "<"
+#
+#1 - Tag detected:
+#   -First we need to find out what type of tag we have:
+#       -if the next character is a "!" we know it is a comment so we switch to state 3
+#       -if the next character is a "/" we know it is a closing tag so we switch to state 4
+#       -unless one of the conditions is true we know it is an opening tag
+#   -if we have an opening tag:
+#       -we continue until either a space or an ">" is met
+#       -then we will create a new Node with the tag being the text between the two pointers
+#       -the new node will be pushed onto the stack
+#       -if the current character is ">" we switch back to state 0
+#       -otherwise we switch to state 2
+#       -lagging pointer is moved to current character
+#       -if we switched to state 0, we have to check if the element we added to the stack is self closing (e.g. <input>, <img>)
+#       -if yes we remove it
+#
+#2 - Reading attributes:
+#   -we continue until a ">" is met
+#   -we pass the string containing all attributes (using the pointers) to another parsing function
+#   -the function returns a dictionary with all attributes that we add to the node on top of the stack
+#   -if the node on top of the stack is self closing, remove it
+#   -we switch back to state 0 and set the lagging pointer to the current character
+#
+#3 - ignoring:
+#   -simply continues until end of comment is found
+#   -lagging pointer is set to current character position
+#   -we switch to state 0
+#
+#4 - Closing tag detected:
+#   -we continue until end of tag is found (">")
+#   -we pop last element from stack
+#   -move lagging pointer to current character position
+#   -switch to state 0
